ef07c2639885581dec5cc3233a967501e0a93d70
robneu <rob@wpsitecare.com>
2015-02-23 11:13:01 -0600

    Made comment nav paginated
    These were previously un-styled and the format was inconsistent with
    other nav elements on the site. This allows them to share the
    pagination styling and markup.

--- theme/comment/navigation.php
+++ theme/comment/navigation.php
@@ -13,18 +13,18 @@
 
 <?php if ( get_option( 'page_comments' ) && 1 < get_comment_pages_count() ) : ?>
 
-	<nav class="comments-nav" role="navigation" aria-labelledby="comments-nav-title">
+	<nav id="comments-nav" class="comments-nav pagination" role="navigation" aria-labelledby="comments-nav-title">
 
 		<h3 id="comments-nav-title" class="screen-reader-text"><?php _e( 'Comments Navigation', 'compass' ); ?></h3>
 
-		<?php previous_comments_link( _x( '&larr; Previous', 'comments navigation', 'compass' ) ); ?>
-
-		<span class="page-numbers"><?php
-			// Translators: Comments page numbers. 1 is current page and 2 is total pages.
-			printf( __( 'Page %1$s of %2$s', 'compass' ), get_query_var( 'cpage' ) ? absint( get_query_var( 'cpage' ) ) : 1, get_comment_pages_count() );
-		?></span>
-
-		<?php next_comments_link( _x( 'Next &rarr;', 'comments navigation', 'compass' ) ); ?>
+		<?php
+		paginate_comments_links(
+			array(
+				'prev_text' => sprintf( '<span class="screen-reader-text">%s</span>' , __( 'Previous Comment Page', 'flagship' ) ),
+				'next_text' => sprintf( '<span class="screen-reader-text">%s</span>', __( 'Next Comment Page', 'flagship' ) ),
+			)
+		);
+		?>
 
 	</nav><!-- .comments-nav -->
 
