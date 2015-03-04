b3539a64fbda64a95877ee33d5ab56c891b498db
robneu <rob@wpsitecare.com>
2015-03-04 16:51:49 -0600

    Split post terms into separate block elements
    This makes the post terms more flexible and allows for a bigger variety
    of display options. Fixes #48

--- theme/content/singular/content.php
+++ theme/content/singular/content.php
@@ -35,12 +35,28 @@
 		<?php wp_link_pages(); ?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<p class="entry-meta">
-			<?php hybrid_post_terms( array( 'taxonomy' => 'category', ) ); ?>
-			<?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', ) ); ?>
-		</p>
-	</footer><!-- .entry-footer -->
+	<?php if ( has_term( '', 'category' ) || has_term( '', 'post_tag' ) ) : ?>
+
+		<footer class="entry-footer">
+			<?php
+			hybrid_post_terms(
+				array(
+					'taxonomy' => 'category',
+					'before'   => '<p class="entry-meta categories">',
+					'after'    => '</p>',
+				)
+			);
+			hybrid_post_terms(
+				array(
+					'taxonomy' => 'post_tag',
+					'before' => '<p class="entry-meta tags">',
+					'after' => '</p>',
+				)
+			);
+			?>
+		</footer><!-- .entry-footer -->
+
+	<?php endif; ?>
 
 	<?php tha_entry_bottom(); ?>
 
