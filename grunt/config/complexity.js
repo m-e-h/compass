6a22c9958a35ec93eaa9092f664244294074a467
robneu <rob@wpsitecare.com>
2015-03-04 01:26:45 -0600

    Simplified complexity options

--- grunt/config/complexity.js
+++ grunt/config/complexity.js
@@ -1,13 +1,7 @@
 // https://github.com/vigetlabs/grunt-complexity
 module.exports = {
 	options: {
-		breakOnErrors: false,
-		errorsOnly: false,
-		cyclomatic: [3, 7, 12],
-		halstead: [8, 13, 20],
-		maintainability: 100,
-		hideComplexFunctions: false,
-		broadcast: false
+		breakOnErrors: false
 	},
 	assets: {
 		src: ['<%= paths.authorAssets %>js/{,*/}*.js']
