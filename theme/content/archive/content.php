bf16296f680b4656c713ab8ceea23b5a349f42a4
robneu <rob@wpsitecare.com>
2015-03-04 17:12:24 -0600

    Moved the standard entry footer to a template part
    The amount of code duplication here was a bit too much for me.

--- theme/content/archive/content.php
+++ theme/content/archive/content.php
@@ -46,28 +46,7 @@
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<?php if ( has_term( '', 'category' ) || has_term( '', 'post_tag' ) ) : ?>
-
-		<footer class="entry-footer">
-			<?php
-			hybrid_post_terms(
-				array(
-					'taxonomy' => 'category',
-					'before'   => '<p class="entry-meta categories">',
-					'after'    => '</p>',
-				)
-			);
-			hybrid_post_terms(
-				array(
-					'taxonomy' => 'post_tag',
-					'before' => '<p class="entry-meta tags">',
-					'after' => '</p>',
-				)
-			);
-			?>
-		</footer><!-- .entry-footer -->
-
-	<?php endif; ?>
+	<?php get_template_part( 'content/parts/entry-footer' ); ?>
 
 	<?php tha_entry_bottom(); ?>
 
--- theme/content/singular/aside.php
+++ theme/content/singular/aside.php
@@ -36,28 +36,7 @@
 		<?php wp_link_pages(); ?>
 	</div><!-- .entry-content -->
 
-	<?php if ( has_term( '', 'category' ) || has_term( '', 'post_tag' ) ) : ?>
-
-		<footer class="entry-footer">
-			<?php
-			hybrid_post_terms(
-				array(
-					'taxonomy' => 'category',
-					'before'   => '<p class="entry-meta categories">',
-					'after'    => '</p>',
-				)
-			);
-			hybrid_post_terms(
-				array(
-					'taxonomy' => 'post_tag',
-					'before' => '<p class="entry-meta tags">',
-					'after' => '</p>',
-				)
-			);
-			?>
-		</footer><!-- .entry-footer -->
-
-	<?php endif; ?>
+	<?php get_template_part( 'content/parts/entry-footer' ); ?>
 
 	<?php tha_entry_bottom(); ?>
 
