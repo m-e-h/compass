ab913f63daa96e79bdb3e8a914da76c442a1010d
robneu <rob@wpsitecare.com>
2015-03-09 04:45:50 -0500

    Split skiplinks out of main theme.js file

--- grunt/config/concat.js
+++ grunt/config/concat.js
@@ -5,6 +5,7 @@ module.exports = {
 			'<%= paths.bower %>fitvids/js/jquery.fitvids.js',
 			'<%= paths.bower %>accessible-menu/js/jquery.accessible-menu.js',
 			'<%= paths.bower %>sidr/js/jquery.sidr.min.js',
+			'<%= paths.authorAssets %>js/skip-links.js',
 			'<%= paths.authorAssets %>js/theme.js'
 		],
 		dest: '<%= paths.theme %>js/theme.js'
