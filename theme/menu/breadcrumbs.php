a645bbb72a05df20d5071831d4598111764ce3f5
robneu <rob@wpsitecare.com>
2015-03-08 22:07:12 -0500

    Removed function exists check
    This function is available regardless of theme support now, so the
    extra check isn’t required

--- theme/menu/breadcrumbs.php
+++ theme/menu/breadcrumbs.php
@@ -10,26 +10,24 @@
  * @since       1.0.0
  */
 
-if ( function_exists( 'flagship_display_breadcrumbs' ) ) {
-	// Display breadcrumbs based on user selections in the customizer.
-	if ( flagship_display_breadcrumbs() ) {
-		// Use Yoast breadcrumbs if they're available.
-		if ( function_exists( 'yoast_breadcrumb' ) ) {
-			yoast_breadcrumb(
-				'<nav class="breadcrumbs" itemprop="breadcrumb">',
-				'</nav>'
-			);
-		}
-		// Fall back to Hybrid Core breadcrumbs if Yoast isn't available.
-		else {
-			breadcrumb_trail(
-				array(
-					'container'     => 'nav',
-					'separator'     => '//',
-					'show_browse'   => false,
-					'show_on_front' => false,
-				)
-			);
-		}
+// Display breadcrumbs based on user selections in the customizer.
+if ( flagship_display_breadcrumbs() ) {
+	// Use Yoast breadcrumbs if they're available.
+	if ( function_exists( 'yoast_breadcrumb' ) ) {
+		yoast_breadcrumb(
+			'<nav class="breadcrumbs" itemprop="breadcrumb">',
+			'</nav>'
+		);
+	}
+	// Fall back to Hybrid Core breadcrumbs if Yoast isn't available.
+	else {
+		breadcrumb_trail(
+			array(
+				'container'     => 'nav',
+				'separator'     => '//',
+				'show_browse'   => false,
+				'show_on_front' => false,
+			)
+		);
 	}
 }
